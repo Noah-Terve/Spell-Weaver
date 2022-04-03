823df84b1297edd52dbce58b2818b7ce4f2ee130
Noah Tervalon <noahtervalon@gmail.com>
2022-04-03 13:37:43 -0400

    Pause Menu

--- Spell_Weavers/Assets/Scripts/GameHandler.cs
+++ Spell_Weavers/Assets/Scripts/GameHandler.cs
@@ -3,20 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour
 {
     // keep track of where the player died so they can respawn from there
     public static string SceneDied = "MainMenu";
     
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    public AudioMixer mixer;
+    public static float volumeLevel = 1.0f;
+    private Slider sliderVolumeCtrl;
+    
+
+    void Awake (){
+        SetLevel (volumeLevel);
+        GameObject sliderTemp = GameObject.FindWithTag("PauseMenuVolumeSlider");
+        if (sliderTemp != null){
+            sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+            sliderVolumeCtrl.value = volumeLevel;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start() {
-        
+        pauseMenuUI.SetActive(false);
+        GameisPaused = false;
+        Spellbook.GameisPaused = false;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameisPaused) Resume();
+            else Pause();
+        }
+    }
+    
+    // pause menu functions
+    void Pause(){
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        GameisPaused = true;
+        Spellbook.GameisPaused = true;
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+        Spellbook.GameisPaused = false;
+    }
+
+    // Music slider in  pause menu
+    public void SetLevel (float sliderValue){
+        mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+        volumeLevel = sliderValue;
     }
     
     // Load the beginning of the game when 
@@ -32,7 +75,22 @@ public class GameHandler : MonoBehaviour
     
     public void ReplayGame() {
         // TODO: add some disincentive to dying.
-        
         SceneManager.LoadScene(SceneDied);
+    }
+    
+    // This function is for the pause menu restart, which brings them back
+    // to the main menu
+    public void RestartGame(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    // This function quits the entire game, again for pause menu.
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 }
