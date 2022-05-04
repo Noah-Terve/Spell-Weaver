b018c624624e1d5f2c6a669ad578a26153aaea70
ThembyG <98064323+ThembyG@users.noreply.github.com>
2022-05-03 21:03:41 -0400

    Slime cannon fix
    Made the slime cannon only fire within a certain distance of the player to be more performant

--- Spell_Weavers/Assets/Scripts/Enemy/EnemySlimeCannon.cs
+++ Spell_Weavers/Assets/Scripts/Enemy/EnemySlimeCannon.cs
@@ -13,22 +13,34 @@ public class EnemySlimeCannon : MonoBehaviour
     public GameObject cannonObject;
     public GameObject projectile;
 
+    private Transform target;
 
     private Vector3 angleVector = Vector2.zero;
     private float timer = 0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         cannonObject.transform.eulerAngles = Vector3.back * angle;
         angleVector = new Vector3(-Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0);
+        if (GameObject.FindGameObjectWithTag ("Player") != null) {
+            target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         timer -= Time.deltaTime;
-        if (timer <= 0)
-            fire();
+        if (timer <= 0) {
+            if (target != null) {
+                float DistToPlayer = Vector3.Distance(transform.position, target.position);
+                if (DistToPlayer <= (2 * distance)) {
+                    fire();
+                }
+            }
+        }
     }
 
     void fire() {
